3f93457788380ad7902d2f51b2c091554563f537
Dipen Parihar <parihar.dipen@gmail.com>
2024-02-01 19:28:19 -0500

    Add form-wtf

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username':'Dipen'}
+    user = {'username':'Class'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign in', form=form)
